26bc66e8a087fb72314f4d11b228f45fa0aa434a
Thiago Hartman <th_guto@hotmail.com>
2021-06-09 01:58:05 -0400

    Importing Poppins font and adding it globally

--- src/styles/global.js
+++ src/styles/global.js
@@ -38,6 +38,15 @@ button{
   cursor: pointer;
 }
 
+body, input, text-area, button {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong{
+  font-weight: 600;
+}
+
 [disabled]{
   opacity: 0.6;
   cursor: not-allowed;
