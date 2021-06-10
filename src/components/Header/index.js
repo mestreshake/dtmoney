cc9ffaccdb89a625dd5a6f3f30d1a94517882a7d
Thiago Hartman <th_guto@hotmail.com>
2021-06-10 06:20:02 -0400

    Creating Header Container

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import logoImg from "../../assets/logo.svg";
+import { Container } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <img src={logoImg} alt="dt money" />
+      <button type="button">Nova transação</button>
+    </Container>
+  );
+}
