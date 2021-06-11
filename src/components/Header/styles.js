c3d2e36402c662ff742a504067b024aabf0c4582
Thiago Hartman <th_guto@hotmail.com>
2021-06-11 05:37:29 -0400

    Header button stylized

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -13,4 +13,20 @@ export const Content = styled.div`
 
   align-items: center;
   justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: var(--shape);
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
