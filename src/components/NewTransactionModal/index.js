6103b1fe80a805663bee6c1f8783ec91cd7cc59f
Thiago Hartman <thiagolhartman@gmail.com>
2021-06-15 17:04:49 -0400

    Modal Card stylized

--- src/components/NewTransactionModal/index.js
+++ src/components/NewTransactionModal/index.js
@@ -1,9 +1,21 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 export function NewTransactionModal({ isOpen, onRequestClose }) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
