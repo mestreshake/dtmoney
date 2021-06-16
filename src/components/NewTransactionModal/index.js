924f242e36414ef2da5c4d1f8fbb85bdbadf7527
Thiago Hartman <thiagolhartman@gmail.com>
2021-06-16 08:00:27 -0400

    Modal button stylized

--- src/components/NewTransactionModal/index.js
+++ src/components/NewTransactionModal/index.js
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { Container, TransactionTypeContainer } from "./styles";
 
 export function NewTransactionModal({ isOpen, onRequestClose }) {
   return (
@@ -17,6 +19,16 @@ export function NewTransactionModal({ isOpen, onRequestClose }) {
         <h2>Cadastrar transação</h2>
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
