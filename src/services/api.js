2247d7e79650ad5fe0a2165fe484969b2e5ee245
Thiago Hartman <thiagolhartman@gmail.com>
2021-07-30 11:06:28 -0400

    [chore]: preparing deploy to netlify

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,4 @@
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:3000/api" });
+export const api = axios.create({
+  baseURL: "https://dtmoneymestreshake.netlify.app/api",
+});
